0aaa3f4d79828a1f689a4909a47211ddeddc5d84
jxio0581 <jxio0581@uni.sydney.edu.au>
2024-11-11 23:35:51 +1100

    Adaptation to window size

--- sketch.js
+++ sketch.js
@@ -1,3 +1,8 @@
+// The code uses Brush() to paint on the canvas. Using brushes of different sizes and transparency, the entire image is drawn by overlaying it with three draws. 
+// The image is adaptive to changes in the window, but because it is loaded with the texture effect of real oil brushes, hundreds of strokes are drawn each time, 
+// so you need to wait a few seconds for the update to be successful. 
+// Each update will change the random properties of all brushes, so the picture will change.
+
 let brushTexture;  //a variable to hold brush texture image
 
 let img;  //a variable to hold picture
@@ -6,206 +11,136 @@ let numSegments = 40;  //divided picture into 40 segments
 
 let segments = [];  //a array to store segments
 
+let savedCanvas;  // store static image of the canvas
+
 function preload() {
   brushTexture = loadImage('assets/brush texture_1.png');
   img = loadImage('assets/Claude_Monet.jpg');
 }
 
 function setup() {
-  createCanvas(img.width, img.height);  //creat canvas based on the picture
-  calculateSegments();
-  background(0);
+  // Initial canvas size based on image
+  resizeCanvasToImage();
+  savedCanvas = createGraphics(width, height);  // Create a graphics cache to save static image
+  calculateSegments();  //calculate segments sizes
+  drawImage();  // Draw image to cache
 }
 
 function draw() {
-    noLoop();  //Do not update the screen
+  image(savedCanvas, 0, 0);  // Display the image on canvas
 
-    //draw the segments(draw the picture stroke by stroke)     
-    //Use a large, dark brush to draw the background
-    for (const segment of segments) {
-      drawStroke(segment.srcImgSegXPos, segment.srcImgSegYPos, segment.srcImgSegWidth, segment.srcImgSegHeight, segment.srcImgSegColour);
-    }
+}
+
+function windowResized() {
+  resizeCanvasToImage();
+  savedCanvas = createGraphics(width, height);  // Recreate graphics cache for new canvas size
+  calculateSegments();  // Recalculate segments based on new canvas size
+  drawImage();  // Redraw image to cache
+  redraw();  // Redraw everything
+}
+
+function resizeCanvasToImage() {
+  // Compare the ratio of the current window to the original image
+  let scaleX = windowWidth / img.width;
+  let scaleY = windowHeight / img.height;
+  let scaleFactor = min(scaleX, scaleY);  // choose the smallest value as the scale value
+
+  // calculate new size while maintain the aspect ratio
+  let newWidth = img.width * scaleFactor;
+  let newHeight = img.height * scaleFactor;
+
+  resizeCanvas(newWidth, newHeight);  // Resize the canvas
 }
 
 function calculateSegments() {
-  segments = [];  // clear existing segments
-  
-  let scaleFactorX = width / img.width;
-  let scaleFactorY = height / img.height;
-  let scaleFactor = min(scaleFactorX, scaleFactorY);  // keep aspect ratio
+  segments = [];  // Clear existing segments
 
-  // calculate the size of each segment
+  let scaleX = width / img.width;
+  let scaleY = height / img.height;
+  let scaleFactor = min(scaleX, scaleY);  // Keep aspect ratio
+
+  // Calculate the size of each segment
   let segmentWidth = (img.width / numSegments) * scaleFactor;
   let segmentHeight = (img.height / numSegments) * scaleFactor;
 
   for (let segYPos = 0; segYPos < height; segYPos += segmentHeight) {
     for (let segXPos = 0; segXPos < width; segXPos += segmentWidth) {
-      // get the colour of the pixel from the image, from the centre of the segment
+      // Get the positions from the center of the segment
       let imgXPos = int(segXPos / scaleFactor);
       let imgYPos = int(segYPos / scaleFactor);
-      let segmentColour = img.get(imgXPos, imgYPos);
+      let segmentColour = img.get(imgXPos, imgYPos);  // Get the color of the pixel from the image, from the center of the segment
       let segment = new ImageSegment(segXPos, segYPos, segmentWidth, segmentHeight, segmentColour);
       segments.push(segment);
     }
   }
 }
 
-function windowResized() {
-  resizeCanvasToImage();
-  calculateSegments();  // Recalculate segments based on new canvas size
-  redraw();  // Redraw everything
+function drawImage() {
+  savedCanvas.background(255);
+  // first layer
+  for (const segment of segments) {
+    Brush(100, 40, 255, savedCanvas, segment.srcImgSegXPos, segment.srcImgSegYPos, segment.srcImgSegWidth, segment.srcImgSegHeight, segment.srcImgSegColour);
+  }
+  // second layer
+  for (const segment of segments) {
+    Brush(60, 20, 200, savedCanvas, segment.srcImgSegXPos, segment.srcImgSegYPos, segment.srcImgSegWidth, segment.srcImgSegHeight, segment.srcImgSegColour);
+  }
+  // third layer
+  for (const segment of segments) {
+    Brush(30, 0, 150, savedCanvas, segment.srcImgSegXPos, segment.srcImgSegYPos, segment.srcImgSegWidth, segment.srcImgSegHeight, segment.srcImgSegColour);
+  }
 }
 
-function resizeCanvasToImage() {
-  let scaleFactorX = windowWidth / img.width;
-  let scaleFactorY = windowHeight / img.height;
-  let scaleFactor = min(scaleFactorX, scaleFactorY);  // Keep the aspect ratio
 
-  // Calculate new dimensions while maintaining the aspect ratio
-  let newWidth = img.width * scaleFactor;
-  let newHeight = img.height * scaleFactor;
-
-  resizeCanvas(newWidth, newHeight);  // Resize the canvas to the new dimensions
-}
-
-
-//a class for getting information and drawing patterns
+// store information of segments
 class ImageSegment {
-  constructor(srcImgSegXPosInPrm,srcImgSegYPosInPrm,srcImgSegWidthInPrm,srcImgSegHeightInPrm,srcImgSegColourInPrm) {
-    //these parameters are used to set the internal properties of an instance of the segment
-    //These parameters are named as imageSource as they are derived from the image we are using
+  constructor(srcImgSegXPosInPrm, srcImgSegYPosInPrm, srcImgSegWidthInPrm, srcImgSegHeightInPrm, srcImgSegColourInPrm) {
     this.srcImgSegXPos = srcImgSegXPosInPrm;
     this.srcImgSegYPos = srcImgSegYPosInPrm;
     this.srcImgSegWidth = srcImgSegWidthInPrm;
     this.srcImgSegHeight = srcImgSegHeightInPrm;
     this.srcImgSegColour = srcImgSegColourInPrm;
   }
-  // draw a stoke
-  // draw1() {
-  //   // draw a temporary canvas by p5.Graphics, because mask() can only be applied to PImage or p5.Graphics
-  //   let pg1 = createGraphics(this.srcImgSegWidth + 100, this.srcImgSegHeight + 30);  // size of the brush
-  //   pg1.fill(this.srcImgSegColour);  // get color from the picture
-  //   pg1.noStroke();
-  //   pg1.rect(0, 0, pg1.width, pg1.height);  // draw a rectangle
-
-  //   // Create a PImage object to store the contents of the temporary canvas
-  //   let pg1Image = createImage(pg1.width, pg1.height);
-  //   pg1Image.copy(pg1, 0, 0, pg1.width, pg1.height, 0, 0, pg1.width, pg1.height);
-
-  //   // Resize the mask image to match the size of pg1
-  //   brushTexture.resize(pg1.width, pg1.height);
-  //   // Apply a mask to the image
-  //   pg1Image.mask(brushTexture);
-
-  //   let angle = random(TWO_PI);  // random angle 
-  //   push(); 
-  //   translate(this.srcImgSegXPos + pg1.width / 2, this.srcImgSegYPos + pg1.height / 2);  // Move the origin to the center of pg1(segments)
-  //   // rotate about the center
-  //   rotate(angle);
-  //   imageMode(CENTER);
-  //   // draw a stroke      
-  //   image(pg1Image, 0, 0);  
-  //   pop(); 
-  //   } 
-
-  // draw2() {
-  //   // Randomize colors, change hue
-  //   let hueShift = random(-30, 30);
-  //   let newColor2 = color(
-  //     constrain(red(this.srcImgSegColour) + hueShift, 0, 255),  // Limit within 255
-  //     constrain(green(this.srcImgSegColour) + hueShift, 0, 255),
-  //     constrain(blue(this.srcImgSegColour) + hueShift, 0, 255),
-  //     200 // little transparency
-  //   );
-
-  //   let pg2 = createGraphics(this.srcImgSegWidth + 50, this.srcImgSegHeight);  // Smaller size
-  //   pg2.fill(newColor2); 
-  //   pg2.noStroke();
-  //   pg2.rect(0, 0, pg2.width, pg2.height); 
-
-  //   let pg2Image = createImage(pg2.width, pg2.height);
-  //   pg2Image.copy(pg2, 0, 0, pg2.width, pg2.height, 0, 0, pg2.width, pg2.height);
-
-  //   brushTexture.resize(pg2.width, pg2.height);
-  //   pg2Image.mask(brushTexture);
-
-  //   let angle = random(TWO_PI); 
-  //   push();
-  //   translate(this.srcImgSegXPos + pg2.width / 2, this.srcImgSegYPos + pg2.height / 2);
-  //   rotate(angle);
-  //   imageMode(CENTER);
-  //   image(pg2Image, 0, 0);
-  //   pop(); 
-  //   }
-
-  // draw3() {
-  //   //More subtle color changes
-  //   let hueShift = random(-15, 15); 
-  //   let newColor3 = color(
-  //     constrain(red(this.srcImgSegColour) + hueShift, 0, 255),
-  //     constrain(green(this.srcImgSegColour) + hueShift, 0, 255),
-  //     constrain(blue(this.srcImgSegColour) + hueShift, 0, 255),
-  //     120
-  //   );
-
-  //   let pg3 = createGraphics(this.srcImgSegWidth + 20, this.srcImgSegHeight);  // Smaller size
-  //   pg3.fill(newColor3);
-  //   pg3.noStroke();
-  //   pg3.rect(0, 0, pg3.width, pg3.height);
-
-  //   let pg3Image = createImage(pg3.width, pg3.height);
-  //   pg3Image.copy(pg3, 0, 0, pg3.width, pg3.height, 0, 0, pg3.width, pg3.height);
-
-  //   brushTexture.resize(pg3.width, pg3.height);
-  //   pg3Image.mask(brushTexture);
-
-  //   let angle = random(TWO_PI);
-  //   push();
-  //   translate(this.srcImgSegXPos + pg3.width / 2, this.srcImgSegYPos + pg3.height / 2);
-  //   rotate(angle);
-  //   imageMode(CENTER);
-  //   image(pg3Image, 0, 0);
-  //   pop(); 
-  //   }
 }
 
-function drawStroke(xPos, yPos, baseWidth, baseHeight, baseColor) {
-  // 随机化尺寸，范围为 0.8 到 1.5 倍
-  // let sizeMultiplier = random(0.8, 1.5);
-  let width = baseWidth;
-  let height = baseHeight;
+// draw a random stroke
+function Brush(widthAdded, heightAdded, transparency, cache, xPos, yPos, baseWidth, baseHeight, baseColor) {
+  // input a number to set the size of brush
+  let width = baseWidth + widthAdded;
+  let height = baseHeight + heightAdded;
 
-  // 随机化颜色，轻微变动色相
+  // Randomize color, shift in hue
   let hueShift = random(-20, 20);
   let newColor = color(
-    constrain(red(baseColor) , 0, 255),
-    constrain(green(baseColor) , 0, 255),
-    constrain(blue(baseColor) , 0, 255),
-    100  // 设置一定透明度
+    constrain(red(baseColor) + hueShift, 0, 255),
+    constrain(green(baseColor) + hueShift, 0, 255),
+    constrain(blue(baseColor) + hueShift, 0, 255),
+    transparency  // input a number to set transparency
   );
 
-  // 创建临时画布绘制图形
-  let pg = createGraphics(width + 100, height + 30);  // size of the brush with some extra margin
+  // Create a temporary canvas to draw the shape by createGraphics()
+  let pg = createGraphics(width, height);  // size of the brush
   pg.fill(newColor);
   pg.noStroke();
   pg.rect(0, 0, pg.width, pg.height);
 
-  // 创建 PImage 对象存储临时画布内容
+  // Create a PImage object to store the contents of the temporary canvas, mask can only be applied to PImage
   let pgImage = createImage(pg.width, pg.height);
   pgImage.copy(pg, 0, 0, pg.width, pg.height, 0, 0, pg.width, pg.height);
 
-  // 调整遮罩大小并应用
+  // Resize the mask image to match the size of pg1
   brushTexture.resize(pg.width, pg.height);
+  // Apply a mask to the image
   pgImage.mask(brushTexture);
 
-  // 随机旋转角度
+  // Randomize rotation angle
   let angle = random(TWO_PI);
-  push();
-  translate(xPos + pg.width / 2, yPos + pg.height / 2);  // 移动到中心
-  rotate(angle);
-  imageMode(CENTER);
-  image(pgImage, 0, 0);  // 在新原点处绘制图像
-  pop();
+  cache.push();
+  cache.translate(xPos + pg.width / 2, yPos + pg.height / 2);  // Move origin to the center
+  cache.rotate(angle);
+  cache.imageMode(CENTER);
+  cache.image(pgImage, 0, 0);  // Draw stroke at the new origin
+  cache.pop();
 }
 
 
